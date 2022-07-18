2ffeda270aceaa8493d0644e63945a214577b46a
Mohammed AbuSamra <93772664+MohammedAbuSamra@users.noreply.github.com>
2022-07-18 22:28:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let count = 0;
 
 function increment() {
   count += 1;
-  countEl.textContent = count;
+  countEl.textContent = count; 
 }
 
 function save() {
@@ -15,3 +15,33 @@ function save() {
 }
 
 console.log("Let's count people on the subway!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
